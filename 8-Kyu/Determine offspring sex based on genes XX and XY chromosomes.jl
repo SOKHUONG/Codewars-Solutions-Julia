f7ccc90596deb4dcd207eb80dc5c801baf9c2233
SOKHUONG <uonsokhuong18@kit.edu.kh>
2020-02-16 09:13:59 +0700

    Added Determine offspring sex based on genes XX and XY chromosomes.jl

---
+++ 8-Kyu/Determine offspring sex based on genes XX and XY chromosomes.jl
@@ -0,0 +1,6 @@
+module Solution
+  export chromosomeCheck
+  function chromosomeCheck(sperm)
+    sperm=="XY" ? "Congratulations! You\'re going to have a son." : "Congratulations! You\'re going to have a daughter."
+  end
+end
